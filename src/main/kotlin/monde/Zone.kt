9a701320eacc25e9fd216bd9c8822be6ab833fb6
Maili Guerry <maili_guerry@laposte.net>
2025-09-30 09:48:48 +0200

    Les TODOs de la classe Zone

--- src/main/kotlin/monde/Zone.kt
+++ src/main/kotlin/monde/Zone.kt
@@ -1,6 +1,10 @@
 package org.example.monde
 
+import org.example.jeu.CombatMonstre
+import org.example.joueur
 import org.example.monstre.EspeceMonstre
+import org.example.monstre.IndividuMonstre
+import kotlin.random.Random
 
 /**
  * Un objet de la Zone peut représenter une route, une caverne, une mer …
@@ -26,7 +30,43 @@ class Zone (
     var zonePrecedente : Zone?
 
 ){
-    // TODO methode genereMonstre()
-    // TODO methode rencontreMonstre()
+    fun genereMonstre(): IndividuMonstre {
+        require(especesMonstres.isNotEmpty()) { "Aucune espèce disponible dans cette zone." }
+        // Choisir une espèce au hasard
+        val especeAleatoire : EspeceMonstre = especesMonstres.random()
+
+        // Générer une variation de +/- 20%
+        val variation : Double = Random.nextDouble(0.8, 1.2)
+        val experienceMonstre : Double = expZone * variation
+
+        // Retourner le monstre généré (monstre sauvage sans entraineur, id 0 et nom de l'espèce par défaut)
+        return IndividuMonstre(
+            id = 0,
+            nom = especeAleatoire.nom,
+            espece = especeAleatoire,
+            entraineur = null,
+            expInit = experienceMonstre
+        )
+    }
+
+    fun rencontreMonstre(){
+        genereMonstre()
+        val monstreSauvage = genereMonstre()
+        var premierMonstre: IndividuMonstre? = null
+
+        for (monstre in joueur.equipeMonstre){
+            if (monstre.pv > 0) {
+                premierMonstre = monstre
+                break
+            }
+        }
+        if (premierMonstre == null) {
+            println("Aucun monstre disponible pour combattre !")
+            return
+        }
+
+        val combat = CombatMonstre(premierMonstre,monstreSauvage)
+        combat.lancerCombat()
+    }
 }
 
