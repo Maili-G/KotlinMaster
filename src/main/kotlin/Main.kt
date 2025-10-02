eb74d56397a00584f929c2cf1d0890d834f80727
Maili Guerry <maili_guerry@laposte.net>
2025-10-02 11:43:30 +0200

    Fin de la classe Partie

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -17,7 +17,7 @@ var rival = Entraineur(2,"Regis",200)
 var especeSpringleaf = EspeceMonstre(1,"Springleaf","graine", 9, 11,10,12,14,60,6.5,9.0,8.0,7.0,10.0,34.0,"Petit monstre espiègle rond comme une graine, adore le soleil","Sa feuille sur sa tête indique son humeur","curieux, amical, timide")
 var especeFlamkip = EspeceMonstre(4,"Flamkip","Animal",12,8,13,16,7,50,10.0,5.5,9.5,9.5,6.5,22.0,"Petit animal entouré de flammes, déteste le froid","Sa flamme change d'intensité selon son énergie","Impulsif, joueur, loyal")
 var especeAquamy = EspeceMonstre(7,"Aquamy","Meteo", 10,11,9,14,14,55,9.0,10.0,7.5, 12.0,12.0,27.0,"Créature vaporeuse semblable à un nuage, produit des gouttes pures.","Fait baisser la température en s’endormant.","Calme, rêveur, mystérieux")
-var route1 = Zone(1,"Prairie",1500,mutableListOf(especeSpringleaf),null, null)
+var route1 = Zone(1,"Prairie",1500,mutableListOf(especeSpringleaf,especeAquamy),null, null)
 var route2 = Zone (2,"Forêt", 1500, mutableListOf(especeFlamkip), null,null)
 var route3 = Zone (3,"Mer", 1500, mutableListOf(especeAquamy), null,null)
 var monstre = Partie(1,joueur,route1)
@@ -36,6 +36,10 @@ fun main() {
     //println(MonsterKube1.utiliser(monstre1))
     val combat = CombatMonstre(monstre1,monstre2)
    println(route1.genereMonstre())
+    //var partie = Partie(1,joueur,route1)
+    //partie.choixStarter()
+    //partie.zone.genereMonstre().afficheDetail()
+
 
 }
 /**
--- src/main/kotlin/jeu/Partie.kt
+++ src/main/kotlin/jeu/Partie.kt
@@ -13,9 +13,12 @@ import org.example.monstre3
 class Partie(
     val id : Int,
     var joueur : Entraineur,
-    val zone : Zone
+    var zone : Zone
 ) {
     fun choixStarter(){
+
+       /** var monstreBis = zone.genereMonstre()
+        var monstre2Bis = zone.genereMonstre()*/
 
         monstre1.afficheDetail()
         monstre2.afficheDetail()
@@ -33,5 +36,61 @@ class Partie(
         starter.entraineur = joueur
     }
 
-    
+    fun modifierOdreEquipe(){
+        println("Entrez la position du monstre que vous voulez déplacer : ")
+        var position1 = readln().toInt()
+
+        println("Entrez la nouvelle position du monstre : ")
+        var position2 = readln().toInt()
+
+        var firstPokemon = joueur.equipeMonstre[position1]
+        var secondPokemon = joueur.equipeMonstre[position2]
+
+        joueur.equipeMonstre[position1] = secondPokemon
+        joueur.equipeMonstre[position2] = firstPokemon
+
+        println(joueur.equipeMonstre)
+    }
+
+    fun examineEquipe(){
+
+        for (monstres in joueur.equipeMonstre){
+            print(joueur.equipeMonstre.indexOf(monstres))
+            monstres.afficheDetail()
+        }
+        println()
+        println("Menu principal : q \n " +
+                "Modifier l'ordre des monstres : m \n " +
+                "Tapez le numéro du monstre pour voir les détails :")
+
+        var choix = readln()
+        if(choix.lowercase() == "q") return
+        else if(choix.lowercase() == "m") modifierOdreEquipe()
+        else if(choix.toInt() in 1..6){
+            var monstre = joueur.equipeMonstre[choix.toInt()-1]
+            monstre.afficheDetail()
+        } else {
+            println("Numero de montre inexistant")
+            examineEquipe()
+        }
+    }
+
+    fun jouer() {
+        println("Vous ete actuellement dans la zone : ${zone.nom}. Vous pouvez :\n" +
+                "1. Rencontrer un monstre sauvage\n" +
+                "2. Examiner l'equipe de monstres\n")
+        if (zone.zoneSuivante != null ) println("3. Aller a la zone suivante")
+        if (zone.zonePrecedente != null ) println("3. Retourner a la zone precedente")
+
+        var action = readln().toInt()
+
+        when (action){
+            1 -> jouer()
+            2 -> examineEquipe()
+            3 -> zone = zone.zoneSuivante!!
+            4 -> zone = zone.zonePrecedente!!
+
+            else -> println("entré invalide")
+        }
+    }
 }
